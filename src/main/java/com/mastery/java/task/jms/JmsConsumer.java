ba11c601e2f978709c4011ae9cc2c63da722217e
Dzmitry Kuhach <aaronkinner2@bk.ru>
2020-12-04 18:07:37 +0300

    Rebuild activemq realisation. Part 2.

--- src/main/java/com/mastery/java/task/jms/JmsConsumer.java
+++ src/main/java/com/mastery/java/task/jms/JmsConsumer.java
@@ -3,7 +3,6 @@ package com.mastery.java.task.jms;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jms.annotation.JmsListener;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,9 +11,7 @@ public class JmsConsumer {
     private final Logger logger = LoggerFactory.getLogger(JmsConsumer.class);
 
     @JmsListener(destination = "employee-message-queue")
-    @SendTo("employee-message-answers-queue")
-    public String receive(String message){
+    public void receive(String message){
         logger.info("Received: " + message);
-        return "Get: " + message;
     }
 }
