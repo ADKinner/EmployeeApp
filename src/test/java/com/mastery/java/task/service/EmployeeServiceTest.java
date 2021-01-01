f1e07c8292bce328383508c3c1f4433cecfc9658
Dzmitry Kuhach <dkaezi@gmail.com>
2021-01-01 15:53:47 +0300

    Rebuild tests for service layer.

--- src/test/java/com/mastery/java/task/service/EmployeeServiceTest.java
+++ src/test/java/com/mastery/java/task/service/EmployeeServiceTest.java
@@ -1,13 +1,17 @@
 package com.mastery.java.task.service;
 
-import com.mastery.java.task.dao.EmployeeDao;
 import com.mastery.java.task.dto.Employee;
+import com.mastery.java.task.exception.EmployeeBaseServiceException;
+import com.mastery.java.task.repository.EmployeeRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 public class EmployeeServiceTest {
@@ -18,25 +22,40 @@ public class EmployeeServiceTest {
     private EmployeeService employeeService;
 
     @Mock
-    private EmployeeDao employeeDao;
+    private EmployeeRepository employeeRepository;
 
     @Test
     public void testGetAllEmployees() {
         employeeService.getAllEmployees();
-        Mockito.verify(employeeDao, Mockito.times(1)).getAll();
+        Mockito.verify(employeeRepository, Mockito.times(1)).findAll();
     }
 
     @Test
-    public void testGetEmployee() {
+    public void testSuccessfulGet() {
+        Mockito.when(employeeRepository.findById(EMPLOYEE_ID)).thenReturn(Optional.of(Mockito.mock(Employee.class)));
         employeeService.getEmployee(EMPLOYEE_ID);
-        Mockito.verify(employeeDao, Mockito.times(1)).get(EMPLOYEE_ID);
+        Mockito.verify(employeeRepository, Mockito.times(1)).findById(EMPLOYEE_ID);
+    }
+
+    @Test
+    public void testUnsuccessfulGet() {
+        Mockito.when(employeeRepository.findById(EMPLOYEE_ID)).thenReturn(Optional.empty());
+        try {
+            employeeService.getEmployee(EMPLOYEE_ID);
+        } catch (EmployeeBaseServiceException exception) {
+            Assertions.assertEquals(
+                    "Could not get employee with id = " + EMPLOYEE_ID,
+                    exception.getMessage()
+            );
+        }
+        Mockito.verify(employeeRepository, Mockito.times(1)).findById(EMPLOYEE_ID);
     }
 
     @Test
     public void testCreateEmployee() {
         Employee employee = Mockito.mock(Employee.class);
         employeeService.createEmployee(employee);
-        Mockito.verify(employeeDao, Mockito.times(1)).create(employee);
+        Mockito.verify(employeeRepository, Mockito.times(1)).save(employee);
     }
 
     @Test
@@ -44,12 +63,12 @@ public class EmployeeServiceTest {
         Employee employee = Mockito.mock(Employee.class);
         employeeService.updateEmployee(employee, EMPLOYEE_ID);
         Mockito.verify(employee).setEmployeeId(EMPLOYEE_ID);
-        Mockito.verify(employeeDao, Mockito.times(1)).update(employee);
+        Mockito.verify(employeeRepository, Mockito.times(1)).save(employee);
     }
 
     @Test
     public void testDeleteEmployees() {
         employeeService.deleteEmployeeById(EMPLOYEE_ID);
-        Mockito.verify(employeeDao, Mockito.times(1)).delete(EMPLOYEE_ID);
+        Mockito.verify(employeeRepository, Mockito.times(1)).deleteById(EMPLOYEE_ID);
     }
 }
